4033237c6ced29bfd66958d1344b905ce1a8ec50
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-07 01:31:48 +0600

    #2: AppRunner class created

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-/**
- * Created by al_imran on 2/6/17.
- */
 public class AppRunner {
+    public static void main(String[] CHAND){
+        Container frame = new Container();
+        frame.setTitle("CI- Calculator");
+        frame.setSize(350, 500);
+        frame.getContentPane().setBackground(Container.windowColor);
+        frame.setLocationRelativeTo(null);
+        //frame.setMaximizedBounds(new Rectangle(300, 200));
+        frame.setResizable(false);
+        ImageIcon img = new ImageIcon("screenshot.png");
+        frame.setIconImage(img.getImage());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
