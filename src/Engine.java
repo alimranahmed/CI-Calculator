6dd3d6c402cd91bd9bc1c645b79160cf5e54aff9
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-08 23:48:17 +0600

    #1: Duplicated code has been moved to Engine class

--- src/Engine.java
+++ src/Engine.java
@@ -1,2 +1,33 @@
 public class Engine {
+    public static double calculate(double number1, double number2, char operation){
+        if (operation == '+') {
+            return number1 + number2;
+        } else if (operation == '-') {
+            return number1 - number2;
+        } else if (operation == '*') {
+            return number1 * number2;
+        } else if (operation == '/') {
+            return number1 / number2;
+        } else if (operation == '%') {
+            return number1 % number2;
+        } else {
+            return number2;
+        }
+    }
+
+    public static String getResultToDisplay(boolean hasPoint, char operation, double result){
+        //when there is any bPoint number or any bDivision operation, there may be a bPoint in the result
+        if (hasPoint || operation == '/')
+        {
+            return "" + result;
+        } else if (!hasPoint) {
+            //when there is no bPoint in the result
+            return "" + (long) result;
+        }
+        return "";
+    }
+
+    public static String getResultToDisplay(boolean hasPoint, double result){
+        return hasPoint ? ""+result : ""+(long)result;
+    }
 }
