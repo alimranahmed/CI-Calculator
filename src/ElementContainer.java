eac549266a95a7fb97f9803a9e89661def733497
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-10 15:06:57 +0600

    #6: all panel's sizes are being determined dynamically

--- src/ElementContainer.java
+++ src/ElementContainer.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 class ElementContainer extends JFrame{
     public Map<String, ButtonHolder> buttonHolderMap;
-    public JPanel screens[] = new JPanel[2];
+    public JPanel screen = new JPanel();
     public JPanel keyPad = new JPanel();
 
     public JTextField outputDisplay = new JTextField();
@@ -18,87 +18,88 @@ class ElementContainer extends JFrame{
         this.setBackground(Helper.darkGrey);
         this.setLayout(null);
 
-        this.screens[0] = new JPanel();
-        this.screens[1] = new JPanel();
-        this.setupDisplay("input");
-        this.setupDisplay("output");
+        this.screen = new JPanel();
+        this.setupDisplay();
 
-        this.setupAllButtons();
+        this.setKeyPad();
 
         //----------------------copyright tag-------------------------------------------------------
         JLabel copyRight = new JLabel("\u00A9 2013  CI- Technology");
-        copyRight.setForeground(Helper.blue);
+        copyRight.setForeground(Helper.BLUE);
         this.keyPad.add(copyRight).setBounds(205, 130, 150, 15);
     }
 
-    private void setupDisplay(String type){
+    private void setupDisplay(){
         System.out.println("[ElementContainer][setupDisplay] screen width: "+this.getWidth());
-        if(type.equalsIgnoreCase("output")){
-            this.screens[1].setLayout(null);
-            this.add(this.screens[1]).setBounds(0, (int)(this.getHeight() * 0.06)+5, this.getWidth(), (int)(this.getHeight() * 0.1));
-            this.screens[1].add(this.outputDisplay);
-            this.outputDisplay.setBounds(0, 0, this.getWidth(), this.screens[1].getHeight());
-            this.outputDisplay.setHorizontalAlignment(JTextField.RIGHT);
-            this.outputDisplay.setFont(Helper.screenFont);
-            this.outputDisplay.setText("0");
-            this.outputDisplay.setEditable(false);
-            this.outputDisplay.setBackground(Helper.green);
-            this.outputDisplay.setForeground(Color.BLACK);
-        }else{
-            this.screens[0].setLayout(null);
-            this.add(this.screens[0]).setBounds(0, 0, this.getWidth(), (int)(this.getHeight() * 0.06));
-            this.screens[0].add(inputDisplay);
-            this.inputDisplay.setBounds(0, 0, this.getWidth(), this.screens[0].getHeight());
-            this.inputDisplay.setHorizontalAlignment(JTextField.LEFT);
-            this.inputDisplay.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-            this.inputDisplay.setText("0");
-            this.inputDisplay.setEditable(false);
-            this.inputDisplay.setBackground(Helper.green);
-            this.inputDisplay.setForeground(Color.BLACK);
-        }
+        this.screen.setLayout(null);
+        int screenHeight = (int)(this.getHeight() * Helper.SCREEN_RATIO);
+        this.add(this.screen).setBounds(0, 0, this.getWidth(), screenHeight);
+        //Input display
+        this.screen.add(inputDisplay);
+        int inputDisplayHeight = (int)(this.screen.getHeight() * Helper.INPUT_DISPLAY_RATIO);
+        this.inputDisplay.setBounds(0, 0, this.screen.getWidth(), inputDisplayHeight);
+        this.inputDisplay.setHorizontalAlignment(JTextField.LEFT);
+        this.inputDisplay.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        this.inputDisplay.setText("0");
+        this.inputDisplay.setEditable(false);
+        this.inputDisplay.setBackground(Helper.GREEN);
+        this.inputDisplay.setForeground(Color.BLACK);
+
+        //Output display
+        this.screen.add(this.outputDisplay);
+        int outputDisplayHeight = (int)(this.screen.getHeight() * Helper.OUTPUT_DISPLAY_RATIO);
+        this.outputDisplay.setBounds(0, inputDisplayHeight, this.screen.getWidth(), outputDisplayHeight);
+        this.outputDisplay.setHorizontalAlignment(JTextField.RIGHT);
+        this.outputDisplay.setFont(Helper.SCREEN_FONT);
+        this.outputDisplay.setText("0");
+        this.outputDisplay.setEditable(false);
+        this.outputDisplay.setBackground(Helper.GREEN);
+        this.outputDisplay.setForeground(Color.BLACK);
     }
 
-    private void setupAllButtons(){
-        //------------setting font, color and style of the common keys-------
+    private void setKeyPad(){
+        //------------setting font, color and style of buttons-------
         ArrayList<String> mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "numeric");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.grey);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.GREY);
 
         mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "operator");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.white);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.WHITE);
 
         mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "number_modifier");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.grey);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.GREY);
 
         mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "answer");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.lightBlue);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.LIGHT_BLUE);
 
         mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "command");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.red);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.RED);
 
         mapKeys = ButtonHolder.getMapKeysByType(this.buttonHolderMap, "single_operator");
-        this.setupButtons(mapKeys, Helper.keyFontMD, Helper.white);
+        this.prepareButtons(mapKeys, Helper.KEY_FONT_MD, Helper.WHITE);
 
-        this.setupButtonByKey("percent", Helper.keyFontMD, Helper.white);
+        this.prepareButtonByKey("percent", Helper.KEY_FONT_MD, Helper.WHITE);
 
-        this.keyPad = new JPanel(); // The keypad that contain the common keys
-        //==========================Setup the keypads=====================
-        this.add(this.keyPad).setBounds(0, 100, this.getWidth(), (int)(this.getHeight() * 1));
+        this.keyPad = new JPanel();
+        int keyPadY = (int)(this.getHeight() * Helper.SCREEN_RATIO);
+        int keyPadHeight = (int)(this.getHeight() * Helper.KEYPAD_RATIO);
+        this.add(this.keyPad).setBounds(0, keyPadY, this.getWidth(), keyPadHeight);
         this.keyPad.setLayout(null);
         this.keyPad.setBackground(Helper.darkGrey);
 
         this.fixButtonsPlacement();
     }
 
-    private void setupButtons(ArrayList<String> buttonHolderMapKeys, Font font, Color bgColor){
+    private void prepareButtons(ArrayList<String> buttonHolderMapKeys, Font font, Color bgColor){
         for(String key : buttonHolderMapKeys){
-            this.setupButtonByKey(key, font, bgColor);
+            this.prepareButtonByKey(key, font, bgColor);
         }
     }
 
-    private void setupButtonByKey(String key, Font font, Color bgColor){
+    private void prepareButtonByKey(String key, Font font, Color bgColor){
         this.buttonHolderMap.get(key).button.setFont(font);
         this.buttonHolderMap.get(key).button.setBackground(bgColor);
         this.buttonHolderMap.get(key).button.setFocusable(false);
+        //this.buttonHolderMap.get(key).button.setBorderPainted(false);
     }
 
     private void fixButtonsPlacement(){
--- src/Helper.java
+++ src/Helper.java
@@ -3,14 +3,19 @@ import java.awt.*;
 public class Helper {
     public final static Color darkGrey = new Color(110, 119, 129);//Color of container window
     //---------------------Fonts & Colors------------------------
-    public final static Font screenFont = new Font("Times New Roman", Font.BOLD, 35);
-    public final static Font keyFontMD = new Font("Times New Roman", Font.PLAIN, 20);
-    public final static Font keyFontSM = new Font("Times New Roman", Font.PLAIN, 15);
+    public final static Font SCREEN_FONT = new Font("Times New Roman", Font.BOLD, 35);
+    public final static Font KEY_FONT_MD = new Font("Times New Roman", Font.PLAIN, 20);
+    public final static Font KEY_FONT_SM = new Font("Times New Roman", Font.PLAIN, 15);
 
-    public final static Color green = new Color(91, 178, 91);
-    public final static Color grey = new Color(212, 212, 212);
-    public final static Color lightBlue = new Color(63, 132, 243);
-    public final static Color red = new Color(224, 67, 67);
-    public final static Color white = new Color(247, 247, 247);
-    public final static Color blue = new Color(0, 0, 255);
+    public final static Color GREEN = new Color(91, 178, 91);
+    public final static Color GREY = new Color(212, 212, 212);
+    public final static Color LIGHT_BLUE = new Color(63, 132, 243);
+    public final static Color RED = new Color(224, 67, 67);
+    public final static Color WHITE = new Color(247, 247, 247);
+    public final static Color BLUE = new Color(0, 0, 255);
+
+    public final static double SCREEN_RATIO = 0.2;
+    public final static double KEYPAD_RATIO = 0.8;
+    public final static double INPUT_DISPLAY_RATIO = 0.3;
+    public final static double OUTPUT_DISPLAY_RATIO = 0.7;
 }
