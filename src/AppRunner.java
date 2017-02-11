5be973924d5ccfbaa6c1014999479560a434dc5a
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-11 22:18:51 +0600

    Unnacessary code removed

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -4,10 +4,8 @@ public class AppRunner {
     public static void main(String[] CHAND){
         ActionServer frame = new ActionServer(500, 600);
         frame.setTitle("CI- Calculator");
-        //frame.setSize(350, 500);
         frame.getContentPane().setBackground(Helper.darkGrey);
         frame.setLocationRelativeTo(null);
-        //frame.setMaximizedBounds(new Rectangle(300, 200));
         frame.setResizable(false);
         ImageIcon img = new ImageIcon("assets/calculator-icon.png");
         frame.setIconImage(img.getImage());
