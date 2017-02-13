309b88b5abb1c521bbefff7b5a3d1d30366c1fda
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-13 21:31:53 +0600

    #12: Exit button exit from system and clear button clear the screen

--- src/ActionServer.java
+++ src/ActionServer.java
@@ -1,19 +1,34 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
-class ActionServer extends ElementContainer implements ActionListener{
+class ActionServer extends ElementContainer implements ActionListener {
 
-     ActionServer(int width, int height){
-         super(width, height);
+    ActionServer(int width, int height) {
+        super(width, height);
         //common keys
-        for(String key : this.buttonHolderMap.keySet()){
-           this.buttonHolderMap.get(key).addActionListener(this);
+        for (String key : this.buttonHolderMap.keySet()) {
+            this.buttonHolderMap.get(key).addActionListener(this);
         }
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Button clicked "+e.getActionCommand());
-        System.out.println(e.getSource());
-        this.inputDisplay.setText(this.inputDisplay.getText()+e.getActionCommand());
+        ButtonHolder pressedButton = (ButtonHolder) e.getSource();
+        System.out.println(new Date()+"[ActionServer][actionPerformed] Button clicked :" + e.getActionCommand()+" "+e.getSource());
+
+        if(pressedButton.pureName.equalsIgnoreCase("exit")){
+            System.exit(0);
+        }else if(pressedButton.pureName.equalsIgnoreCase("clear")){
+            this.clearScreen();
+        }else{
+            this.inputDisplay.setText(this.inputDisplay.getText() + e.getActionCommand());
+        }
+    }
+
+    private void clearScreen(){
+        this.inputDisplay.setText("0");
+        this.outputDisplay.setText("0");
     }
 }
