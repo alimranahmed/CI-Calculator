5ac91e6fd051aaef628ccf79a7be30ee9235226f
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-18 00:20:32 +0600

    #13: Tan is being calculated

--- src/Engine.java
+++ src/Engine.java
@@ -1,40 +1,40 @@
-import java.util.DoubleSummaryStatistics;
+import java.util.Arrays;
 
 class Engine {
-    String computeSciFun(String funcName, String param){
+    String computeSciFun(String funcName, String param) {
         double paramValue = Double.parseDouble(param);
 
-        if(funcName.equalsIgnoreCase("square")){
-            return ""+(paramValue * paramValue);
+        if (funcName.equalsIgnoreCase("square")) {
+            return "" + (paramValue * paramValue);
 
-        }else if(funcName.equalsIgnoreCase("cube")){
-            return ""+(paramValue * paramValue * paramValue);
+        } else if (funcName.equalsIgnoreCase("cube")) {
+            return "" + (paramValue * paramValue * paramValue);
 
-        }else if(funcName.equalsIgnoreCase("square_root")){
-            return ""+(Math.sqrt(paramValue));
+        } else if (funcName.equalsIgnoreCase("square_root")) {
+            return "" + (Math.sqrt(paramValue));
 
-        }else if(funcName.equalsIgnoreCase("one_by_n")){
-            return ""+((double)1/paramValue);
+        } else if (funcName.equalsIgnoreCase("one_by_n")) {
+            return "" + ((double) 1 / paramValue);
 
-        }else if(funcName.equalsIgnoreCase("sin")){
-            return ""+this.calculateTrigonometry("sin", paramValue);
+        } else if (Arrays.asList("sin", "cos", "tan").contains(funcName)) {
+            return "" + this.calculateTrigonometry(funcName, paramValue);
 
-        }else if(funcName.equalsIgnoreCase("cos")){
-            return ""+this.calculateTrigonometry("cos", paramValue);
         }
         return "Not implemented yet";
     }
 
-    private double calculateTrigonometry(String functionName, double param){
-        if(functionName.equalsIgnoreCase("sin")) {
+    private double calculateTrigonometry(String functionName, double param) {
+        if (functionName.equalsIgnoreCase("sin")) {
             return Math.sin(Math.toRadians(param));
-        }else if(functionName.equalsIgnoreCase("cos")){
+        } else if (functionName.equalsIgnoreCase("cos")) {
             return Math.cos(Math.toRadians(param));
+        } else if (functionName.equalsIgnoreCase("tan")) {
+            return Math.tan(Math.toRadians(param));
         }
         return param;
     }
 
-    static double calculate(double number1, double number2, char operation){
+    static double calculate(double number1, double number2, char operation) {
         if (operation == '+') {
             return number1 + number2;
         } else if (operation == '-') {
@@ -50,10 +50,9 @@ class Engine {
         }
     }
 
-    static String getResultToDisplay(boolean hasPoint, char operation, double result){
+    static String getResultToDisplay(boolean hasPoint, char operation, double result) {
         //when there is any bPoint number or any bDivision operation, there may be a bPoint in the result
-        if (hasPoint || operation == '/')
-        {
+        if (hasPoint || operation == '/') {
             return "" + result;
         } else {
             //when there is no bPoint in the result
@@ -61,7 +60,7 @@ class Engine {
         }
     }
 
-    static String getResultToDisplay(boolean hasPoint, double result){
-        return hasPoint ? ""+result : ""+(long)result;
+    static String getResultToDisplay(boolean hasPoint, double result) {
+        return hasPoint ? "" + result : "" + (long) result;
     }
 }
