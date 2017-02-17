5285b9124d75de7fadcda4e0de0963bc90b39d82
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-18 00:33:32 +0600

    Unnacessary Scientific element has been removed

--- src/ButtonHolder.java
+++ src/ButtonHolder.java
@@ -54,9 +54,9 @@ class ButtonHolder extends JButton{
         map.put("square", new ButtonHolder("single_operator", "square", "x\u00B2", "x\u00B2"));
         map.put("cube", new ButtonHolder("single_operator", "cube", "x\u00B3", "x\u00B3"));
         map.put("sqrt", new ButtonHolder("single_operator", "square_root", "\u221A", "\u221A"));
-        String[] strings = {"sin", "cos", "tan", "asin", "acos", "atan", "sinh", "cosh", "tanh", "log", "log"};
-        //map.put("ten_power", new ButtonHolder("single_operator", "ten_power", "10^n", "10^n"));
-        //map.put("abs", new ButtonHolder("single_operator", "absolute", "abs", "abs"));
+        String[] strings = {"sin", "cos", "tan", "asin", "acos", "atan", "sinh", "cosh", "tanh", "ln", "log"};
+        map.put("ten_power", new ButtonHolder("single_operator", "ten_power", "10^n", "10^n"));
+        map.put("abs", new ButtonHolder("single_operator", "absolute", "abs", "abs"));
         for (String text : strings) {
             map.put(text, new ButtonHolder("single_operator", text, text, text));
         }
