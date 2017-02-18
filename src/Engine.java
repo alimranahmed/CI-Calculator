ae6f8711706a3ba89d8d491331bf046ebcefeb26
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-19 00:13:16 +0600

    Code refactored

--- src/Engine.java
+++ src/Engine.java
@@ -1,42 +1,43 @@
 import java.util.Arrays;
 
 class Engine {
-    String computeSciFun(String funcName, String param) {
-        double paramValue = Double.parseDouble(param);
+    String computeSciFun(String funcName, String input) {
+        double inputValue = Double.parseDouble(input);
 
         if (funcName.equalsIgnoreCase("square")) {
-            return "" + (paramValue * paramValue);
+            return "" + (inputValue * inputValue);
 
         } else if (funcName.equalsIgnoreCase("cube")) {
-            return "" + (paramValue * paramValue * paramValue);
+            return "" + (inputValue * inputValue * inputValue);
 
         } else if (funcName.equalsIgnoreCase("square_root")) {
-            return "" + (Math.sqrt(paramValue));
+            return "" + (Math.sqrt(inputValue));
 
         } else if (funcName.equalsIgnoreCase("one_by_n")) {
-            return "" + ((double) 1 / paramValue);
+            return "" + ((double) 1 / inputValue);
 
         }else if(funcName.equalsIgnoreCase("ln")){
-            return ""+Math.log(paramValue);
+            return ""+Math.log(inputValue);
 
         } else if(funcName.equalsIgnoreCase("log")){
-            return ""+Math.log10(paramValue);
+            return ""+Math.log10(inputValue);
 
         }else if (Arrays.asList("sin", "cos", "tan").contains(funcName)) {
-            return "" + this.calculateTrigonometry(funcName, paramValue);
+            return "" + this.calculateTrigonometry(funcName, inputValue);
 
         }
         return "Not implemented yet";
     }
 
-    private double calculateTrigonometry(String functionName, double param) {
+    private double calculateTrigonometry(String functionName, double input) {
+        double radianInput = Math.toRadians(input);
         if (functionName.equalsIgnoreCase("sin")) {
-            return Math.sin(Math.toRadians(param));
+            return Math.sin(radianInput);
         } else if (functionName.equalsIgnoreCase("cos")) {
-            return Math.cos(Math.toRadians(param));
+            return Math.cos(radianInput);
         } else if (functionName.equalsIgnoreCase("tan")) {
-            return Math.tan(Math.toRadians(param));
+            return Math.tan(radianInput);
         }
-        return param;
+        return input;
     }
 }
